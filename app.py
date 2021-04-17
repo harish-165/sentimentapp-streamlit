f8c46672916d6d19ed0bc41eed9864304e99ace3
harish-165 <76102106+harish-165@users.noreply.github.com>
2021-04-18 01:23:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+import numpy as np
 import joblib
 model =joblib.load('Project_Data')
 st.title('Sentiment Analysis')
