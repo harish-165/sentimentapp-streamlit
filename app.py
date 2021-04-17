f9eb58483fffb5ae60184c0a53d12d39c0f76d2e
harish-165 <76102106+harish-165@users.noreply.github.com>
2021-04-18 00:59:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+model =joblib.load('Project_Data')
+st.title('Sentiment Analysis')
+ip = st.text_input('Enter your message')
+op = model.predict([ip])
+if st.button('predict'):
+  st.title(op[0])
